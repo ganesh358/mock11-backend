296af65bdc1337025f2e5b9c0be28a5d987612e4
ganesh358 <ganeshmoremc@gmail.com>
2023-01-19 15:30:58 +0530

    Sf

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const { LoginRoute } = require("./Routes/loginRoute")
 const app = express()
 
 app.use(express.json())
+
 app.use(cors())
+
 
 app.use("/signup", SignUpRoute)
 app.use("/login", LoginRoute)
